e24b61a9bc77527f61f3ff5e5733914d71c4648c
Julia Knura <julia.knura25@gmail.com>
2025-03-23 21:47:04 +0100

    Add loose cors rules

--- src/main/java/com/example/cloud_notes_backend/controllers/ImageNotesController.java
+++ src/main/java/com/example/cloud_notes_backend/controllers/ImageNotesController.java
@@ -5,15 +5,13 @@ import com.example.cloud_notes_backend.services.ImageNotesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 public class ImageNotesController {
 
     private final ImageNotesService service;
--- src/main/java/com/example/cloud_notes_backend/controllers/TextNotesController.java
+++ src/main/java/com/example/cloud_notes_backend/controllers/TextNotesController.java
@@ -6,14 +6,12 @@ import com.example.cloud_notes_backend.services.TextNotesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 public class TextNotesController {
 
     private final TextNotesService service;
